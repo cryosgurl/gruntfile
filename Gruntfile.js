fdd93d1fb6af731b0fff5406dbfbf4ef012fcf0d
Mandy <mandy@competa.com>
2017-05-04 10:54:42 +0200

    made a better single page one

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,98 @@
-
 module.exports = function(grunt) {
-	var path = require('path');
-	
-	require('load-grunt-config')(grunt, {
-    configPath: path.join(process.cwd(), 'grunt/config'),
-    jitGrunt: {
-      customTasksDir: 'grunt/tasks'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded',
+          sourcemap: 'none'
+        },
+        files: {
+          'style.css': 'sass/global.scss',      
+          'css/dev.style.css': 'sass/global.scss',
+        }
+      }
     },
-    data: {
-      foo: 'bar' // accessible with '<%= foo %>'
-    }
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')(),
+          require('rucksack-css')({ fallbacks: true })
+        ]
+      },
+      dist: {
+        src: 'style.css',
+        dest: 'style.css'
+      },
+      dev: {
+        src: 'css/dev.style.css',
+        dest: 'css/dev.style.css'
+      },
+    },
+    cssmin: {
+      target: {
+        files: {
+          'style.css': 'style.css'
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          'js/lib/no-conflict.js',
+          'js/lib/skip-navigation.js',
+        ],
+        dest: 'js/scripts.js'
+      },
+    },
+    jshint: {
+      files: [
+        'js/scripts.js',
+        'js/ie.js',
+      ],
+      options: {
+        scripturl: true,
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        compress: true,
+        quoteStyle: 3
+      },
+      dist: {
+        files: {
+          'js/head.min.js': 'js/head.js',
+          'js/scripts.min.js': 'js/scripts.js',
+          'js/ie.min.js'     : 'js/ie.js',
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['js/**/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false
+        }
+      },
+      css: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass', 'postcss', 'cssmin']
+      }
+    },
   });
-};	
-
-
-	
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsvalidate');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  
+  grunt.registerTask('default', ['jshint','uglify','sass','postcss','concat','cssmin','watch']);
+};
--- grunt/config/watch.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-watch: {	
-		files: ['src//*.css', 'src//*.scss'], 
-		tasks: ['sass', 'cssmin'] 
-	} 
-};
